8163b24a7c306015fe801057374b5a914a0a4e19
nabil alaa <nabilalaa06@gmail.com>
2022-01-02 10:22:29 +0200

    ‏‏restaurant

--- main/models.py
+++ main/models.py
@@ -19,3 +19,13 @@ class Order(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Calc(models.Model):
+    name = models.CharField(max_length=100,null=True)
+    price = models.FloatField(null=True)
+    total = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.name
+
--- main/views.py
+++ main/views.py
@@ -1,35 +1,54 @@
 from django.shortcuts import render, HttpResponseRedirect
 from .models import *
+import math
 
 
 def index(request):
+    data = request.POST
+    print(data)
+    name = data.get("name")
+    quantity = data.get("quantity")
+    price = data.get("price")
     if request.method == "POST":
-        data = request.POST
-        print(data)
-
-        name = data.get("name")
-        quantity = data.get("quantity")
-        price = data.get("price")
-        Order.objects.create(name=name, quantity=quantity, price=float(price)*int(quantity))
-
-        HttpResponseRedirect("home")
+        if name and quantity and price:
+            Order.objects.create(name=name, quantity=quantity, price=float(price) * int(quantity))
+            Calc.objects.create(name=name, price=price)
     context = {
         "chicken": Menu.objects.filter(category="chicken"),
         "meat": Menu.objects.filter(category="meat"),
         "fries": Menu.objects.filter(category="fries"),
-        "menus": Menu.objects.all()
+        "menus": Menu.objects.all(),
+
     }
     return render(request, "index.html", context)
 
 
 def receipt(request):
+
+    print("____________")
+    print(Calc.objects.all())
+
+    Tlist = []
+    for i in Calc.objects.all():
+        print(i.price)
+        Tlist.append(i.price)
+        print(Tlist)
+
+    t = math.fsum(Tlist)
+
+    print(t)
+
+    Calc.objects.update(total=t)
+    name = request.POST.get("name")
+    price = request.POST.get("price")
+    if request.method == "POST":
+        Calc.objects.update(total= float(t) - float(price))
+        Order.objects.filter(name=name).delete()
+        Calc.objects.filter(name=name).delete()
     context = {
         "orders": Order.objects.all(),
-        "order": Order.objects.count()
+        "order": Order.objects.count(),
+        "total": Calc.objects.last()
+
     }
-    data = request.POST.get("name")
-
-    if request.POST:
-        Order.objects.filter(name=data).delete()
-
     return render(request, "receipt.html", context)
